8559f48d8c8cfff072e504dbab7b6172c0261e09
Marco Carraro <carraromarco89@gmail.com>
2015-07-23 10:28:10 +0200

    Modified rosnect2 and deleted some useless files.

--- src/rosnect2.cpp
+++ src/rosnect2.cpp
@@ -28,32 +28,40 @@
 #include <iostream>
 #include <signal.h>
 
+
+#include <ros/ros.h>
+
 #include <opencv2/opencv.hpp>
 
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/frame_listener_impl.h>
 
+#include <pcl_ros/point_cloud.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
+
 bool protonect_shutdown = false;
 
-  libfreenect2::FrameMap frames;
+libfreenect2::FrameMap frames;
 
 
 libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth);
-  
-  libfreenect2::Freenect2 freenect2;
-  libfreenect2::Freenect2Device *dev = freenect2.openDefaultDevice();
+
+libfreenect2::Freenect2 freenect2;
+libfreenect2::Freenect2Device *dev = freenect2.openDefaultDevice();
 
 void sigint_handler(int s)
 {
-  protonect_shutdown = true;
+    protonect_shutdown = true;
 }
 
 void my_handler(int s){
-          protonect_shutdown = true;
-           printf("Caught signal %d\n",s);
-           listener.release(frames);
-           dev->stop();
-  dev->close();
+    protonect_shutdown = true;
+    printf("Caught signal %d\n",s);
+    listener.release(frames);
+    dev->stop();
+    dev->close();
+    ros::shutdown();
     exit(1);
 
 
@@ -61,59 +69,85 @@ void my_handler(int s){
 
 int main(int argc, char *argv[])
 {
-  
+    ros::init(argc,argv,"rosnect2");
 
-  if(dev == 0)
-  {
-    std::cout << "no device connected or failure opening the default one!" << std::endl;
-    return -1;
-  }
+    if(dev == 0)
+    {
+        std::cout << "no device connected or failure opening the default one!" << std::endl;
+        return -1;
+    }
 
-  struct sigaction sigIntHandler;
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
 
-   sigIntHandler.sa_handler = my_handler;
-   sigemptyset(&sigIntHandler.sa_mask);
-   sigIntHandler.sa_flags = 0;
-
-   sigaction(SIGINT, &sigIntHandler, NULL);
-
-  //signal(SIGINT,sigint_handler);
-  protonect_shutdown = false;
+    //signal(SIGINT,sigint_handler);
+    protonect_shutdown = false;
 
 
-  dev->setColorFrameListener(&listener);
-  dev->setIrAndDepthFrameListener(&listener);
-  dev->start();
+    dev->setColorFrameListener(&listener);
+    dev->setIrAndDepthFrameListener(&listener);
+    dev->start();
 
-  std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
-  std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+    std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+    std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
 
-  while(!protonect_shutdown)
-  {
-    listener.waitForNewFrame(frames);
-    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
-    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+    ros::NodeHandle nh;
+    ros::Publisher cloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB> >
+            ("/points",1);
 
-    unsigned char **pprgba = reinterpret_cast<unsigned char **>(rgb->data);
-    cv::Mat rgba(1080, 1920, CV_8UC4, pprgba[0]);
-    cv::Mat bgra(1080, 1920, CV_8UC4);
-    cv::cvtColor(rgba, bgra, cv::COLOR_RGBA2BGRA);
-    cv::imshow("rgb", bgra);
-    cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
-    cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
+    cv::Mat color, irMat, depthMat, ir, depth;
 
-    int key = cv::waitKey(1);
-    protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
+//    pcl::visualization::PCLVisualizer viewer;
+    while(!protonect_shutdown)
+    {
+        listener.waitForNewFrame(frames);
+//        libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+//        libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+        libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
 
-    listener.release(frames);
-    //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
-  }
+//        color = cv::Mat(1080, 1920, CV_8UC4, (reinterpret_cast<unsigned char **>(rgb>data))[0]);
+//        irMat = cv::Mat(ir->height, ir->width, CV_32FC1, ir->data);
+//        depthMat = cv::Mat(depth->height, depth->width, CV_32FC1, depth->data);
 
-  // TODO: restarting ir stream doesn't work!
-  // TODO: bad things will happen, if frame listeners are freed before dev->stop() :(
-  dev->stop();
-  dev->close();
+//        unsigned char **pprgba = reinterpret_cast<unsigned char **>(rgb->data);
+//        cv::Mat rgba(1080, 1920, CV_8UC4, pprgba[0]);
+//        cv::Mat bgra(1080, 1920, CV_8UC4);
+//        cv::cvtColor(rgba, bgra, cv::COLOR_RGBA2BGRA);
+//        cv::imshow("rgb", bgra);
+//        cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+//        cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
 
-  return 0;
+//        cv::flip(irMat, ir, 1);
+//        cv::flip(depthMat, depth, 1);
+
+//        int key = cv::waitKey(1);
+//        protonect_shutdown = protonect_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
+
+        cloud = (*depth->cloud).makeShared();
+
+//        viewer.removeAllPointClouds();
+//        viewer.resetCamera();
+//        viewer.addPointCloud(cloud);
+//        viewer.spin();
+
+        listener.release(frames);
+        //libfreenect2::this_thread::sleep_for(libfreenect2::chrono::milliseconds(100));
+
+        cloud_pub.publish(*cloud);
+
+        ros::spinOnce();
+        //ros::Duration(0.005).sleep();
+    }
+
+    // TODO: restarting ir stream doesn't work!
+    // TODO: bad things will happen, if frame listeners are freed before dev->stop() :(
+    dev->stop();
+    dev->close();
+    ros::shutdown();
+
+    return 0;
 }
